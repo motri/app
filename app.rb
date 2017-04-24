d6830411880d4fa08089be69813ea5f39d4be710
Unai Motriko <unaimotriko@gmail.com>
2017-04-24 15:36:30 +0100

    Names the cat randomly

--- app.rb
+++ app.rb
@@ -1,9 +1,16 @@
 require 'sinatra'
 require 'shotgun'
-#set :session_secret, 'super secret'
 
-get '/' do
-  "<div style='border: 3px dashed red'>
-      <img src='http://bit.ly/1eze8aE'>
-     </div>"
+
+ get '/' do
+ end
+
+
+
+get '/cat' do
+  erb(:index)
+  #erb "Hi there, Visitor <%= 2 + 2 %>!"
 end
+
+
+set :session_secret, 'super secret'
