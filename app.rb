89f985360f4c6ad02bff78248b832590bf393907
Unai Motriko <unaimotriko@gmail.com>
2017-04-24 15:07:40 +0100

    Sinatra loads a cat photo

---
+++ app.rb
@@ -0,0 +1,9 @@
+require 'sinatra'
+require 'shotgun'
+#set :session_secret, 'super secret'
+
+get '/' do
+  "<div style='border: 3px dashed red'>
+      <img src='http://bit.ly/1eze8aE'>
+     </div>"
+end
